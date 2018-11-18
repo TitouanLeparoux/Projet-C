6c1f3ca43ecfb58abeff4320a70d86b564f23e8a
TitouanLeparoux <titouan.leparoux@gmail.com>
2018-11-18 12:04:20 +0100

    [KEY] move "sur place"
    This reverts commit 5ecb1b53c1e24902daab520e3a17059a91364b71.

--- src/sdl/control_key.c
+++ src/sdl/control_key.c
@@ -1,36 +1,38 @@
 int control_key(int continuer)
 {
   SDL_Event event;
+  int save_dir = 0;
 
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
       case SDL_QUIT: continuer = 0; break;
       case SDL_KEYDOWN: 
+        save_dir = TK_user->direction;
         switch (event.key.keysym.sym) {
         
         case SDLK_ESCAPE: continuer = 0; break;
         
         case SDLK_DOWN:
           TK_user->direction = 2;
-          if(MAP[(TK_user->posY+SCL)/SCL][TK_user->posX/SCL].type != 'w' ) {
+          if(save_dir == TK_user->direction && MAP[(TK_user->posY+SCL)/SCL][TK_user->posX/SCL].type != 'w' ) {
             TK_user->posY += SCL; }
           break;
         
         case SDLK_LEFT:
           TK_user->direction = 4;
-          if(MAP[(TK_user->posY)/SCL][(TK_user->posX-SCL)/SCL].type != 'w' ) {
+          if(save_dir == TK_user->direction && MAP[(TK_user->posY)/SCL][(TK_user->posX-SCL)/SCL].type != 'w' ) {
             TK_user->posX -= SCL; }
           break;
         
         case SDLK_RIGHT:
           TK_user->direction = 6; 
-          if(MAP[(TK_user->posY)/SCL][(TK_user->posX+SCL)/SCL].type != 'w' ) {
+          if(save_dir == TK_user->direction && MAP[(TK_user->posY)/SCL][(TK_user->posX+SCL)/SCL].type != 'w' ) {
             TK_user->posX += SCL; }
           break;
         
         case SDLK_UP:
           TK_user->direction = 8;
-          if(MAP[(TK_user->posY-SCL)/SCL][TK_user->posX/SCL].type != 'w' ) {
+          if(save_dir == TK_user->direction && MAP[(TK_user->posY-SCL)/SCL][TK_user->posX/SCL].type != 'w' ) {
             TK_user->posY -= SCL; }
           break;
         
